ee8f438f106843dbdda50eca8726f28c85f9cede
Rayhan <prog.rayhan1@gmail.com>
2023-01-01 16:44:30 +0600

    objects added

---
+++ 01-objects.py
@@ -0,0 +1,12 @@
+# object = eatch registered form are called object
+# class = collection of objects
+
+# class
+class Student:
+    roll = ""
+    gpa = ""
+
+# objects (created from class)
+rayhan = Student()
+# rayhan is the object of Student class
+# now rayhan have roll & gpa fields

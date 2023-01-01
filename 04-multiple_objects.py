f013802917b5b7b7d7d018fd9342b3743e2f1ec0
Rayhan <prog.rayhan1@gmail.com>
2023-01-01 19:01:06 +0600

    multiple objects added

---
+++ 04-multiple_objects.py
@@ -0,0 +1,18 @@
+class Student:
+    roll = ""
+    gpa = ""
+
+# object 1
+arif = Student()
+arif.gpa = 9.2
+
+print(arif.gpa)
+
+
+# object 2
+niloy = Student()
+niloy.roll = 1212
+niloy.gpa = 4.9
+
+print(niloy.roll)
+print(niloy.gpa)

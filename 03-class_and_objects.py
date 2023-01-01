5099ef0d5fbd35cd6d887e873c9722fcdc5d3e4c
Rayhan <prog.rayhan1@gmail.com>
2023-01-01 18:50:54 +0600

    class & objects added

---
+++ 03-class_and_objects.py
@@ -0,0 +1,13 @@
+class Student:
+    roll = ""
+    gpa = ""
+
+rahim = Student()
+# now rahim can use all the property of Student class
+# at this moment, rahim object has roll & gpa property
+# so,
+rahim.roll = 1653
+rahim.gpa = 4.5
+
+print(rahim.roll)
+print(rahim.gpa)
